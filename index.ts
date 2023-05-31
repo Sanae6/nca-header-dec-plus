5988a2ff3f26386d9ace29b81aa43febc0bf2ec1
Sanae <atmatm6000@outlook.com>
2023-05-31 17:57:39 -0400

    Add key area decryption

--- index.ts
+++ index.ts
@@ -6,7 +6,10 @@ function sliceHelper(view: ArrayBufferView, length?: number) {
     return view.buffer.slice(view.byteOffset, view.byteOffset + (length ?? view.byteLength));
 }
 
-const decBinds: { decrypt(key: ArrayBufferLike, header: ArrayBufferLike): ArrayBuffer } = bindings({});
+const decBinds: {
+    decryptHeader(key: ArrayBufferLike, header: ArrayBufferLike): ArrayBuffer,
+    decryptArea(key: ArrayBufferLike, chunk: ArrayBufferLike): ArrayBuffer
+} = bindings({});
 
 export function decryptNcaHeader(key: BufferOrView, header: BufferOrView): Uint8Array {
     if (key.byteLength !== 0x20)
@@ -18,5 +21,22 @@ export function decryptNcaHeader(key: BufferOrView, header: BufferOrView): Uint8
     if (ArrayBuffer.isView(header))
         header = sliceHelper(header, 0xC00);
 
-    return new Uint8Array(decBinds.decrypt(key, header));
+    return new Uint8Array(decBinds.decryptHeader(key, header));
+}
+
+export function decryptNcaKeyArea(key: BufferOrView, area: BufferOrView[]) {
+    if (key.byteLength !== 0x10)
+        throw new Error(`'key' must be 0x10 bytes long, got ${key.byteLength}`);
+    if (ArrayBuffer.isView(key))
+        key = sliceHelper(key);
+
+    const k = key;
+
+    return area.map((chunk) => {
+        if (chunk.byteLength !== 0x10)
+            throw new Error("element of 'area' must of 0x10 bytes long, got " + chunk.byteLength)
+        if (ArrayBuffer.isView(chunk))
+            chunk = sliceHelper(chunk);
+        return new Uint8Array(decBinds.decryptArea(k, chunk));
+    })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
+use aes::{Aes128, cipher::generic_array::GenericArray, cipher::KeyInit};
+use ecb::cipher::BlockDecryptMut;
 use neon::prelude::*;
 use neon::types::buffer::TypedArray;
-use aes::{Aes128, cipher::KeyInit, cipher::generic_array::GenericArray};
-use xts_mode::{Xts128};
+use xts_mode::Xts128;
 
 pub fn get_nintendo_tweak(sector_index: u128) -> [u8; 0x10] {
     sector_index.to_be_bytes()
@@ -35,8 +36,32 @@ fn decrypt_nca_header(mut cx: FunctionContext) -> JsResult<JsArrayBuffer> {
     Ok(output_buffer)
 }
 
+type Aes128EcbDec = ecb::Decryptor<aes::Aes128>;
+
+fn decrypt_nca_area(mut cx: FunctionContext) -> JsResult<JsArrayBuffer> {
+    let mut key_buffer = [0; 0x10];
+    key_buffer.copy_from_slice(cx.argument::<JsArrayBuffer>(0)?.as_slice(&cx));
+    let buffer = cx.argument::<JsArrayBuffer>(1)?;
+    let slice = buffer.as_slice(&mut cx);
+    // let buffer = cx.argument::<JsArrayBuffer>(2)?;
+
+    println!("SLice len {}", slice.len());
+    let mut ecb = Aes128EcbDec::new(&key_buffer.into());
+    let mut out_slice = [0; 0x10];
+    ecb.decrypt_block_b2b_mut(slice.into(), (&mut out_slice).into());
+    // for s in slice.chunks_mut(0x10) {
+    //     println!("test {:02X?}", s);
+    //     ecb.decrypt_block_mut(s.into());
+    // }
+
+    let mut output_buffer = cx.array_buffer(0x10)?;
+    output_buffer.as_mut_slice(&mut cx).copy_from_slice(&out_slice);
+    Ok(output_buffer)
+}
+
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("decrypt", decrypt_nca_header)?;
+    cx.export_function("decryptHeader", decrypt_nca_header)?;
+    cx.export_function("decryptArea", decrypt_nca_area)?;
     Ok(())
 }
